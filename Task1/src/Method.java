420da63d5f764165479eb1974d43836e24812b49
DarkDrinker <Anders.wade.jensen@hotmail.com>
2023-03-27 12:46:05 +0200

    Udkast

---
+++ Task1/src/Method.java
@@ -0,0 +1,23 @@
+public class Method {
+
+    static void methodA(){
+        System.out.print("J");
+        System.out.print("a");
+        System.out.print("v");
+        System.out.print("a ");
+    }
+    static void methodB(){
+        System.out.print("e");
+        System.out.print("r ");
+    }
+
+    static void methodC() {
+        System.out.print("s");
+        System.out.print("j");
+        System.out.print("o");
+        System.out.print("v");
+        System.out.print("t");
+    }
+
+
+}
